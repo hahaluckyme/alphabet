8bdb56ce6238f28d111ece946928e77c68c5bfcc
Ereshkigal Rikan <admin@radiantamaranth.com>
2019-02-12 01:18:33 -0800

    Changed scene loading code to always capitalize the module name from the file name to fix case sensitivity issues

--- src/codegen/scenes.js
+++ src/codegen/scenes.js
@@ -3,6 +3,7 @@ const glob = require('glob');
 const files = glob.sync('./src/data/scenes/**/*.js');
 
 module.exports = files.map(filepath => {
-  const module_name = filepath.split('/').pop().split('.')[0];
-  return `export ${module_name} from 'data/scenes/${module_name}'`;
+  const file_name = filepath.split('/').pop().split('.')[0];
+  const module_name = file_name.charAt(0).toUpperCase() + file_name.slice(1); 
+  return `export ${module_name} from 'data/scenes/${file_name}'`;
 }).join('\n');
--- src/codegen/scenes.mock.js
+++ src/codegen/scenes.mock.js
@@ -4,7 +4,8 @@ const files = glob.sync('./src/data/scenes/**/*.js');
 
 const map = {};
 files.forEach(filepath => {
-  const module_name = filepath.split('/').pop().split('.')[0];
+  const file_name = filepath.split('/').pop().split('.')[0];
+  const module_name = file_name.charAt(0).toUpperCase() + file_name.slice(1); 
   map[module_name] = module_name;
 });
 module.exports = map;
